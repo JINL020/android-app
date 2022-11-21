2144f0eb202e23ddbbe7bfb36e53a7c586b03611
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2022-11-21 04:29:56 +0100

    created function for onClick of button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void disable(View v){
+        v.setEnabled(false);
+        Button button = (Button)v;
+        button.setText("disabled");
+        TextView text = findViewById(R.id.textView);
+        text.setText("You clicked the button");
     }
 }
